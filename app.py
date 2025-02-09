f6cd933b17e5122c4ff591edbb2a899b047d6916
b-fujino <b-fujino@g.fpu.ac.jp>
2025-02-09 19:42:52 +0900

    streamlitを試したけどうまくいかず．Macの設定がよくわからん．

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title('Hello Streamlit in Google Colab!')
+    st.write('This is a sample Streamlit app running in Google Colab.')
+
+if __name__ == '__main__':
+    main()
