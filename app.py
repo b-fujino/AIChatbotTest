27c741098738f618230c2ccc56629ba3a3bc7a72
b-fujino <b-fujino@g.fpu.ac.jp>
2025-02-15 14:59:49 +0900

    テキスト入力と保持

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+
 import streamlit as st
 
-def main():
-    st.title('Hello Streamlit in Google Colab!')
-    st.write('This is a sample Streamlit app running in Google Colab.')
+st.title('インタフェースを整える')
 
-if __name__ == '__main__':
-    main()
+# 初期化
+allinputs = ""
+
+# テキストを入力
+input_text = st.text_input('Input a text')
+st.write('You input:', input_text)
+
+# 過去の入力も表示
+allinputs += input_text+"  \n" # 改行するにはスペース2つと改行文字を入れる 
+st.write('All inputs:', allinputs)
