e091f2569c213adfcbdabb6cb419cfbbd43085a9
b-fujino <b-fujino@g.fpu.ac.jp>
2025-02-18 14:06:02 +0900

    Lumちゃん化してみた

--- chat.py
+++ chat.py
@@ -1,8 +1,11 @@
 
 from dotenv import load_dotenv
-import os
+#import os
 from openai import OpenAI
 import streamlit as st
+
+# アシスタントの画像設定
+assistant_image_url = "Lum.png"
 
 # 環境ファイルからOPENAI_APIを読み込む
 load_dotenv()
@@ -10,22 +13,42 @@ load_dotenv()
 # OpenAI のクライアントを取得
 client = OpenAI()
 
-# チャット履歴をセッションに保存
+# sytemプロンプトの作成
+systemprompt = """
+あなたは「うる星やつら」の登場人物のラムちゃんです．
+あなたは彼氏のことをダーリンと呼びます．
+あなたの彼氏の名前は諸星あたるです．
+あなたは友引町に住んでいます．
+あなたは鬼族の娘です．
+あなたは父親と共に侵略者として地球に来ましたが、鬼ごっこで諸星あたるに負けて地球侵略をあきらめました．
+あなたは侵略しないかわりに、諸星あたると婚約し、地球に住むようになりました．
+あなたは友引高校に通っています．
+あなたはの友達は、面倒終太郎、さくら先生、三宅しのぶ、ラン、お雪、弁天です．
+さくら先生のお父さんは錯乱坊です．
+あなたはしゃべるときには「だっちゃ」や「っちゃ」といった形の仙台弁を使います．
+あなたは自分のことを「うち」と呼びます．
+"""
+
+# チャット履歴を保存するセッション変数の作成
 if "messages" not in st.session_state:
     st.session_state.messages = [
-        {"role": "system", "content": "あなたは「うる星やつら」の登場人物のラムちゃんです."},
+        {"role": "system", "content": systemprompt},
     ]
 
 
-st.title("Chat App with Streamlit")
+st.title("Let’s Chat with Lum-chan!")
 
 # チャット履歴を表示
 last_role = ""
 with st.container():
     for message in st.session_state.messages:
         if message["role"] == "system":continue # systemプロンプトは表示しない
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        elif message["role"] == "user":
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+        elif message["role"] == "assistant":
+            with st.chat_message(message["role"], avatar=assistant_image_url):
+                st.markdown(message["content"])
         last_role = message["role"]
 
 
@@ -33,7 +56,7 @@ with st.container():
 if last_role == "user":
     # OpenAI に送信
     completion = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model="gpt-4o",
         messages=st.session_state.messages
     )
     system_response = completion.choices[0].message.content
